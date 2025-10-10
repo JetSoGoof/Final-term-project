4ecc4370b339461f6725fc9bfb32fa9866960d93
Jxz887 <jazz0656871156@gmail.com>
2025-10-10 15:31:46 +0700

    fixmain

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+import os
+import tkinter as tk
+import customtkinter as ctk
+from PIL import Image
+
+from utils import resource_path
+from tabs.split_tab import SplitTab
+from tabs.txn_tab import TxnTab
+from tabs.sav_tab import SavTab
