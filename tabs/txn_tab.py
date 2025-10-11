a1ba60dad86bb0ebdc7df171f7aef7b8c78aba08
Sarun <comes1177@gmail.com>
2025-10-11 20:31:42 +0700

    class 1 com

--- tabs/txn_tab.py
+++ tabs/txn_tab.py
@@ -15,3 +15,19 @@ class AnimatedNumberLabel(ctk.CTkLabel):
         self.animation_steps = 20
         self.animation_id = None
 
+    def animate_to(self, target):
+        if self.animation_id: self.after_cancel(self.animation_id)
+        self.target_value = target
+        self.step = (self.target_value - self.current_value) / self.animation_steps
+        self._update_animation()
+
+    def _update_animation(self):
+        if (self.step > 0 and self.current_value < self.target_value) or \
+           (self.step < 0 and self.current_value > self.target_value):
+            self.current_value += self.step
+            self.configure(text=money(self.current_value))
+            self.animation_id = self.after(15, self._update_animation)
+        else:
+            self.current_value = self.target_value
+            self.configure(text=money(self.target_value))
+            self.animation_id = None
