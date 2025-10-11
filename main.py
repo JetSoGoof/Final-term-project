7d863b4513a2d263a2b263eb01c3db43bd80fc90
Jirat <jirat.nakchean@gmail.com>
2025-10-11 14:53:47 +0700

    TAE 1

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ from utils import resource_path
 from tabs.split_tab import SplitTab
 from tabs.txn_tab import TxnTab
 from tabs.sav_tab import SavTab
+
+# Windows Taskbar Icon Fix
+if sys.platform.startswith("win"):
+    import ctypes
+    MY_APP_ID = u"MoneyHyper.App.2.0"
+    try:
+        ctypes.windll.she1132.SetCurrentProcessExplicitAppUserModelID(MY_APP_ID)
+    except Exception:
+        pass
