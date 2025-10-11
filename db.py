97e068b9b0c421de1215fb679227bd2fe56d31e0
Ahgil Phomchoo <ahgil2549@gmail.com>
2025-10-12 01:41:36 +0700

    shaw3

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
-DB_PATH = os.environ.get("MONEYHYPE_DB")
-if not DB_PATH:
-    DATA_DIR = user_data_dir()
-    os.makedirs(DATA_DIR, exist_ok=True)
-    DB_PATH = os.path.join(DATA_DIR, "db.json")
+db = TinyDB(DB_PATH)
+T_TXNS = db.table("transactions")
+T_SAV  = db.table("savings")
+T_SPL  = db.table("splits")
+Q = Query()
